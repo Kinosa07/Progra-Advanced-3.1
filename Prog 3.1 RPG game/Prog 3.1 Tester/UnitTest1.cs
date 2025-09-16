396f1e6c214c802ec601d83580646ccfa39128e4
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-16 15:40:42 +0200

    Ajout Prog 3.1 Tester
    Addition d'un projet test "Prog 3.1 Tester" qui aura pour but de tester le programme, càd "Prog 3.1 RPG game"

---
+++ Prog 3.1 RPG game/Prog 3.1 Tester/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Prog_3._1_Tester
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

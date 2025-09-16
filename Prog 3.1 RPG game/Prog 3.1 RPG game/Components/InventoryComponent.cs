0fafb05f138c9141c099f3ad633c7bedcb01bad5
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-16 15:45:07 +0200

    Création InventoryComponent + Folder Component
    Création de la classe InventoryComponent et création du folder Components

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/InventoryComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3._1_RPG_game.Components
+{
+    internal class InventoryComponent
+    {
+    }
+}

e75f4c0cb94482544caf348a3261cc236dfa6f1b
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-17 17:39:22 +0200

    Création Notes.cs
    Espace de prises de notes personnels pour écrire ce que j'ai prévu pour le projet avant de l'oublier.
    Dépourvu de code

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Notes.cs
@@ -0,0 +1,32 @@
+﻿/*Espace pour les notes personneles lors de la création du projet. NE RIEN CODER DEDANS!!!
+ * 
+ * GameLoop: Facteur. Income: Point A > Point B.
+ * 
+ * Addition fight ennemis quand pas sur les senties battus (Forcer le joueur a sortir des sentiers battus)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+*/

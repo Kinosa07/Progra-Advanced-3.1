15f58598d31014638308f2164dc8f9fb67fc25a8
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-17 18:25:39 +0200

    Création d'un RenderComponent
    Création d'un Component RenderComponent ayant un PositionComponent et une apparence comme éléments internes

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/RenderComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3._1_RPG_game.Components
+{
+    internal class RenderComponent : Component
+    {
+        private PositionComponent _positionComponent;
+        private string _appearance;
+
+        public RenderComponent()
+        {
+
+        }
+
+        public override void Update(float time_since_last_update)
+        {
+
+        }
+
+        public override void FixedUpdate(float fixed_update_time)
+        {
+            
+        }
+    }
+}

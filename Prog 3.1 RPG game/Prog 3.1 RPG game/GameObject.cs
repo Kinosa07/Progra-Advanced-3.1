a6ad5923082a18bc565904da45c6236d65c29290
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-17 17:00:02 +0200

    Création de la classe GameObject
    Création de la classe GameObject en lui donnant un constructeur par défaut et un tableau de component

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using Prog_3._1_RPG_game.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3._1_RPG_game
+{
+    public class GameObject
+    {
+        private Component[] _componenttable = new Component[5];
+
+        public GameObject() 
+        {
+            
+        }
+    }
+}

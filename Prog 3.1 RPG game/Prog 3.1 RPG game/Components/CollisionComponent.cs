32d582f59480cc5e3285ce5e4f657058b76fe409
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-19 21:25:14 +0200

    Création CollisionComponent
    Création d'un CollisionComponent qui a pour but d'empêcher le joueur de se trouver sur une case

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/CollisionComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3._1_RPG_game.Components
+{
+    internal class CollisionComponent : Component
+    {
+        private PositionComponent _positionComponent = new PositionComponent(-1, -1);
+
+        private int _previousColliderPosX;
+        private int _previousColliderPosY;
+
+        //Ce que CollCompo doit faire: Repousser les objets vers leurs positions initiales : Get Previous X & Y et utiliser Movement Component
+        //Comment trouver si un objet collisione???
+
+        public override void Update(float deltaTime)
+        {
+            //Analyser si Collisions
+            //Si Collision, enregistrer previous position Collider
+        }
+
+        public override void FixedUpdate(float fixed_update_time)
+        {
+            //Repousser les objets au positions de base
+        }
+    }
+}

bbd06b6ba8cc7adde380417111883c873530a481
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-19 21:36:34 +0200

    Création MapComponent
    Création d'un Component qui a pour but de contenir tout les objets présents sur la map donnée (World contiendrait des villes, City contiendrait des bâtiments, and so on)

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/MapComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3._1_RPG_game.Components
+{
+    internal class MapComponent : Component
+    {
+        GameObject[] _mapContentsTable = new GameObject[4];
+
+
+    }
+}

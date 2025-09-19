4e54fa85009baf26224ee31bd1ab4d7aa6cbadc2
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-19 21:35:45 +0200

    Dépendences Constructeur CollisionComponent
    Ajout des dépendances explicites lors de la création de CollisionComponent

--- Prog 3.1 RPG game/Prog 3.1 RPG game/Components/CollisionComponent.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/CollisionComponent.cs
@@ -9,12 +9,19 @@ namespace Prog_3._1_RPG_game.Components
     internal class CollisionComponent : Component
     {
         private PositionComponent _positionComponent = new PositionComponent(-1, -1);
+        private MapComponent _mapComponent = new MapComponent();
 
         private int _previousColliderPosX;
         private int _previousColliderPosY;
 
+        public CollisionComponent (PositionComponent object_position_component, MapComponent location_map_component)
+        {
+            _positionComponent = object_position_component;
+            _mapComponent = location_map_component;
+        }
+
         //Ce que CollCompo doit faire: Repousser les objets vers leurs positions initiales : Get Previous X & Y et utiliser Movement Component
-        //Comment trouver si un objet collisione???
+        //Comment trouver si un objet collisione: Utiliser MapComponent et parcourir TOUT les objets présents
 
         public override void Update(float deltaTime)
         {

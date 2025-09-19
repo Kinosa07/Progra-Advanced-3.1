81de81eeea30e9a371ddf95f17e6560f689c2587
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-19 22:06:58 +0200

    création Logique Collision
    Logique d'Update pour vérifier les collisions
    
    Lignes 34 et 40 vérifie si la position d'un élément présent sur la map actuelle et l'objet lui même collisione
    Dans ce cas, le code enregistre la position dans le tableau des éléments de la map.
    La ligne 47 se produit lorsqu'un élément collisione avec l'objet ET si une position dans le tableau des éléments présents dans la map actuelle. Elle enregistre la position précédente du Collider
    La ligne 32 crée une boucle qui parcourt tout le contenu du tableau d'objets présents sur la map actuelle

--- Prog 3.1 RPG game/Prog 3.1 RPG game/Components/CollisionComponent.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/CollisionComponent.cs
@@ -13,6 +13,9 @@ namespace Prog_3._1_RPG_game.Components
 
         private int _previousColliderPosX;
         private int _previousColliderPosY;
+        int _tablePositionOfCollider = -1;
+
+        private bool _isColliding;
 
         public CollisionComponent (PositionComponent object_position_component, MapComponent location_map_component)
         {
@@ -26,7 +29,27 @@ namespace Prog_3._1_RPG_game.Components
         public override void Update(float deltaTime)
         {
             //Analyser si Collisions
+            for (int map_object_index = 0; map_object_index < _mapComponent.GetAllObjectsInside().Length; map_object_index++)
+            {
+                _tablePositionOfCollider = map_object_index;
+                if (_positionComponent.GetPositionX() == _mapComponent.GetAllObjectsInside()[map_object_index].GetComponent<PositionComponent>().GetPositionX())
+                {
+                    _isColliding = true;
+                    _tablePositionOfCollider = map_object_index;
+                }
+
+                if (_positionComponent.GetPositionY() == _mapComponent.GetAllObjectsInside()[map_object_index].GetComponent<PositionComponent>().GetPositionY())
+                {
+                    _isColliding = true;
+                    _tablePositionOfCollider = map_object_index;
+                }
+            }
             //Si Collision, enregistrer previous position Collider
+            if (_isColliding && (_tablePositionOfCollider >= 0))
+            {
+                _previousColliderPosX = _mapComponent.GetAllObjectsInside()[_tablePositionOfCollider].GetComponent<PositionComponent>().GetPreviousPositionX();
+                _previousColliderPosY = _mapComponent.GetAllObjectsInside()[_tablePositionOfCollider].GetComponent<PositionComponent>().GetPreviousPositionY();
+            }
         }
 
         public override void FixedUpdate(float fixed_update_time)

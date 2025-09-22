a5cfe1903b373e9497452a8c458cabf0cde6ad53
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-22 12:08:20 +0200

    Update Notes.cs

--- Prog 3.1 RPG game/Prog 3.1 RPG game/Notes.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Notes.cs
@@ -2,7 +2,7 @@
  * 
  * GameLoop: Facteur. Income: Point A > Point B.
  * 
- * Addition fight ennemis quand pas sur les senties battus (Forcer le joueur a sortir des sentiers battus)
+ * Addition fight ennemis quand pas sur les senties battus (& Forcer le joueur a sortir des sentiers battus)
  * 
  * Component List[ Inventaire:OK, Movement:pas OK, Position:OK, Render:pas OK, Collision:OKish, Map: pas OK]
  * 
@@ -17,12 +17,13 @@
  * 
  * 
  * State Machine
- * 
- * 
+ * 1 State par Type de Location (Shop/Inn, City, country, Planet, ...)
+ * Qui gère la sortie du village/shop/inn etc...
  * 
  * Event Manager
  * 
- * 
+ * RenderManager??? Et que fait RenderCompo si RenderManager existe
+ * Fonctionnemtn Render: Parcourir un tableau de case. Ecrire " " si la case est vide et utiliser le render d'un objet a sa position
  * 
  * 
  * 

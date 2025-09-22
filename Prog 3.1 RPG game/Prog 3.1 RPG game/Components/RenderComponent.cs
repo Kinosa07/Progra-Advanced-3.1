d6a9fad6120ff0db2d71f66948e0e2269379fce8
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-22 12:21:15 +0200

    Augmentation du nombre de sprites
    Création d'un second constructeur qui utilise quatre images pour des objets pouvant avoir plusieurs orientations.
    Ajout d'un Dictionnaire qui gère les sprites de l'objet

--- Prog 3.1 RPG game/Prog 3.1 RPG game/Components/RenderComponent.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/RenderComponent.cs
@@ -9,12 +9,21 @@ namespace Prog_3._1_RPG_game.Components
     internal class RenderComponent : Component
     {
         private PositionComponent _positionComponent;
-        private string _appearance;
+        private Dictionary<string, string> _appearanceTable;
 
-        public RenderComponent(PositionComponent position_component, string wanted_appearance)
+
+        public RenderComponent(PositionComponent position_component, string immobile_image)
         {
             _positionComponent = position_component;
-            _appearance = wanted_appearance;
+            _appearanceTable.Add("base", immobile_image);
+        }
+        public RenderComponent(PositionComponent position_component, string looking_up, string looking_down, string looking_left, string looking_right)
+        {
+            _positionComponent = position_component;
+            _appearanceTable.Add("Up", looking_up);
+            _appearanceTable.Add("Down", looking_down);
+            _appearanceTable.Add("Left", looking_left);
+            _appearanceTable.Add("Right", looking_right);
         }
 
         public override void Update(float time_since_last_update)

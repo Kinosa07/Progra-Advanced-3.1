5d137084e395f5e76fa7deb8f2be2d80e97edcd2
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-25 20:30:19 +0200

    Création tableau d'objets bordure a l'initialisation
    Création d'un tableau de GameObject équipés de position, renders et collision à la création d'un map Component
    Et assignation de la colonne du haut

--- Prog 3.1 RPG game/Prog 3.1 RPG game/Components/MapComponent.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/MapComponent.cs
@@ -9,6 +9,7 @@ namespace Prog_3._1_RPG_game.Components
     internal class MapComponent : Component
     {
         GameObject[] _mapContentsTable = new GameObject[4];
+        private GameObject[] _mapBordersTable = new GameObject[1];
         //créer des bordures de map
         private int _mapSizeX;
         private int _mapSizeY;
@@ -17,7 +18,23 @@ namespace Prog_3._1_RPG_game.Components
         {
             _mapSizeX = map_size_x;
             _mapSizeY = map_size_y;
+            _mapBordersTable = new GameObject[(2 * _mapSizeX) + (2 * _mapSizeY) - 4];
 
+            for (int border_table_index = 0; border_table_index < _mapBordersTable.Length; border_table_index++)
+            {
+                _mapBordersTable[border_table_index] = new GameObject();
+            }
+
+            for (int horizontal_map_size_index = 0; horizontal_map_size_index > _mapSizeX; horizontal_map_size_index++)
+            {
+                PositionComponent game_object_position_component = new PositionComponent(horizontal_map_size_index, 0);
+                CollisionComponent game_object_collision_component = new CollisionComponent(game_object_position_component, this);
+                RenderComponent game_object_render_component = new RenderComponent(game_object_position_component, "=");
+
+                _mapBordersTable[horizontal_map_size_index].AddComponent(game_object_position_component);
+                _mapBordersTable[horizontal_map_size_index].AddComponent(game_object_collision_component);
+                _mapBordersTable[horizontal_map_size_index].AddComponent(game_object_render_component);
+            }
         }
 
         public override void Update(float time_since_last_update)

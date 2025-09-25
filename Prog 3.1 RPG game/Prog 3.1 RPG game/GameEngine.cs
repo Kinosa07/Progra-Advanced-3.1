544b2916ef90a028c526db608ebe0e199a0af19f
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-25 21:06:08 +0200

    Disorganized Commit GameEngine
    Ajout de fonctions variées dans Game Engine en fonction de leurs actions voulue (presque aucun code fonctionnel)

--- Prog 3.1 RPG game/Prog 3.1 RPG game/GameEngine.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/GameEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,42 @@ namespace Prog_3._1_RPG_game
 {
     internal class GameEngine
     {
+        private bool _shouldQuit = false;
+        private Stopwatch _stopWatch = new Stopwatch();
+        private float _lastUpdateTime = 0;
+        public void Run()
+        {
+            while (!_shouldQuit)
+            {
+                ProcessInput();
+                Update(_lastUpdateTime);
+                FixedUpdate(1.0f);
+                Render();
+                Thread.Sleep(1000 / 10);
+            }
+        }
 
+        public void Render()
+        {
+
+        }
+
+        public void Update(float time_since_last_update)
+        {
+
+        }
+
+        public void FixedUpdate(float fixed_time_until_update)
+        {
+            if (fixed_time_until_update <= )
+            {
+
+            }
+        }
+
+        public void ProcessInput()
+        {
+
+        }
     }
 }

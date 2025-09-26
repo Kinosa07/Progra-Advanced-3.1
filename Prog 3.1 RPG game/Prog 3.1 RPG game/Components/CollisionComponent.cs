e8c6847ddbbe083eb2d4fe1c35649f715b89ef5e
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-26 19:49:24 +0200

    CollisionComponent: Migration de la logique
    Migration de la logique de collision vers un Manager

--- Prog 3.1 RPG game/Prog 3.1 RPG game/Components/CollisionComponent.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/CollisionComponent.cs
@@ -8,9 +8,7 @@ namespace Prog_3._1_RPG_game.Components
 {
     internal class CollisionComponent : Component
     {
-        private PositionComponent _positionComponent = new PositionComponent(-1, -1);
-        //RETOURNER ICI PAS OK
-        private MapComponent _mapComponent = new MapComponent(new RenderManager(),2,2);
+        private PositionComponent _positionComponent = new PositionComponent(1, 1);
 
         private int _previousColliderPosX;
         private int _previousColliderPosY;
@@ -28,37 +26,12 @@ namespace Prog_3._1_RPG_game.Components
 
         public override void Update(float deltaTime)
         {
-            //Analyser si Collisions
-            for (int map_object_index = 0; map_object_index < _mapComponent.GetAllObjectsInside().Length; map_object_index++)
-            {
-                _tablePositionOfCollider = map_object_index;
-                if (_positionComponent.GetPositionX() == _mapComponent.GetAllObjectsInside()[map_object_index].GetComponent<PositionComponent>().GetPositionX())
-                {
-                    _isColliding = true;
-                    _tablePositionOfCollider = map_object_index;
-                }
-
-                if (_positionComponent.GetPositionY() == _mapComponent.GetAllObjectsInside()[map_object_index].GetComponent<PositionComponent>().GetPositionY())
-                {
-                    _isColliding = true;
-                    _tablePositionOfCollider = map_object_index;
-                }
-            }
-            //Si Collision, enregistrer previous position Collider
-            if (_isColliding && (_tablePositionOfCollider >= 0))
-            {
-                _previousColliderPosX = _mapComponent.GetAllObjectsInside()[_tablePositionOfCollider].GetComponent<PositionComponent>().GetPreviousPositionX();
-                _previousColliderPosY = _mapComponent.GetAllObjectsInside()[_tablePositionOfCollider].GetComponent<PositionComponent>().GetPreviousPositionY();
-            }
+            
         }
 
         public override void FixedUpdate(float fixed_update_time)
         {
-            //Repousser les objets au positions de base
-            if (_isColliding && (_tablePositionOfCollider >= 0))
-            {
-                _mapComponent.GetAllObjectsInside()[_tablePositionOfCollider].GetComponent<MovementComponent>().MoveObject(_previousColliderPosX, _previousColliderPosY);
-            }
+            
         }
     }
 }

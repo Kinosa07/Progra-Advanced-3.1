7c5b8466bb8f49bea173e2a816c4c6658404f101
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-26 16:13:46 +0200

    Créaiton stuff multiple RenderManager
    Création tableau contenant les rendercomponents gérés
    
    Création d'un constructeur vide (a modifier??)
    
    Création d'une fonction pour ajout un RenderComponent (Manque la partie d'ajout du Component)

--- Prog 3.1 RPG game/Prog 3.1 RPG game/RenderManager.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/RenderManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prog_3._1_RPG_game.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,42 @@ namespace Prog_3._1_RPG_game
 {
     internal class RenderManager
     {
+        private RenderComponent[] _renderComponentsCollection = new RenderComponent[3];
 
+        public RenderManager()
+        {
+
+        }
+
+        public void AddRenderComponent(RenderComponent render_component_to_add)
+        {
+            //Is table Full
+            bool is_table_full = true;
+            for (int collection_index = 0; collection_index < _renderComponentsCollection.Length; collection_index++)
+            {
+                if (_renderComponentsCollection[collection_index] == null)
+                {
+                    is_table_full = false;
+                    break;
+                }
+            }
+
+            if (is_table_full == true)
+            {
+                //magic to expand Table
+                RenderComponent[] temporary_table = new RenderComponent[_renderComponentsCollection.Length + 3];
+                for (int collection_index = 0; collection_index < _renderComponentsCollection.Length; collection_index++)
+                {
+                    temporary_table[collection_index] = _renderComponentsCollection[collection_index];
+                }
+                _renderComponentsCollection = temporary_table;
+                is_table_full = false;
+            }
+
+            if (is_table_full == false)
+            {
+                //NOT FINISHED
+            }
+        }
     }
 }

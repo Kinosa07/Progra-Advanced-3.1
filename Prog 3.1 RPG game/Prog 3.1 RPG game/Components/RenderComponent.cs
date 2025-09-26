7744606bb76a6ff2d4340f757f924150f10e1e9b
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-26 18:08:24 +0200

    Ajout dépendance RenderManager
    Ajout de la dépendance d'un RenderManager lors de la construction d'un RenderComponent

--- Prog 3.1 RPG game/Prog 3.1 RPG game/Components/RenderComponent.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/RenderComponent.cs
@@ -12,13 +12,13 @@ namespace Prog_3._1_RPG_game.Components
         private Dictionary<string, string> _appearanceTable = new Dictionary<string, string>();
         private string _currentLookingDirection;
 
-        public RenderComponent(PositionComponent position_component, string immobile_image)
+        public RenderComponent(RenderManager game_render_manager ,PositionComponent position_component, string immobile_image)
         {
             _positionComponent = position_component;
             _appearanceTable.Add("base", immobile_image);
             _currentLookingDirection = "base";
         }
-        public RenderComponent(PositionComponent position_component, string looking_up, string looking_down, string looking_left, string looking_right)
+        public RenderComponent(RenderManager game_render_manager, PositionComponent position_component, string looking_up, string looking_down, string looking_left, string looking_right)
         {
             _positionComponent = position_component;
             _appearanceTable.Add("up", looking_up);

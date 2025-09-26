8085660df3b84938d1a7eb6f2fd15342ad691c57
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-26 16:05:52 +0200

    GameEngine dans Program & Logique d'update du joueur
    Implémentation d'un GameEngine dans la classe Program & Implémentation de la logique d'Update et FixedUpdate du personnage joueur

--- Prog 3.1 RPG game/Prog 3.1 RPG game/GameEngine.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/GameEngine.cs
@@ -37,6 +37,7 @@ namespace Prog_3._1_RPG_game
         public void Update(float time_since_last_update)
         {
             _lastUpdateTime = _stopWatch.ElapsedMilliseconds;
+            _player.Update(time_since_last_update);
         }
 
         public void FixedUpdate(float fixed_time_until_update)
@@ -44,6 +45,7 @@ namespace Prog_3._1_RPG_game
             if (fixed_time_until_update <= _lastUpdateTime)
             {
                 //Work your magic
+                _player.FixedUpdate(_lastUpdateTime);
             }
         }
 

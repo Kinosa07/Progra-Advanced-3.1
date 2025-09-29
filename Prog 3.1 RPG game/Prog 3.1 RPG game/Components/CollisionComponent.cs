86dc47b2a53066cc18ad957730c35d04083c0edf
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-29 10:22:33 +0200

    Collisioncomponent: Dépendance Partielle Move
    Ajout d'un second constructeur avec un MovementComponent comme dépéndance pour tout les objets avec un collisioncomponent et un MovementComponent

--- Prog 3.1 RPG game/Prog 3.1 RPG game/Components/CollisionComponent.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/CollisionComponent.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Prog_3._1_RPG_game.Components
     internal class CollisionComponent : Component
     {
         private PositionComponent _positionComponent = new PositionComponent(1, 1, new GameObject());
+        private MovementComponent _movementComponent = null;
 
         private int _previousColliderPosX;
         private int _previousColliderPosY;
@@ -20,6 +22,14 @@ namespace Prog_3._1_RPG_game.Components
         public CollisionComponent (PositionComponent object_position_component, GameObject parent)
         {
             _positionComponent = object_position_component;
+            _movementComponent = null;
+            _parentGameObject = parent;
+        }
+
+        public CollisionComponent (PositionComponent object_position_component, MovementComponent object_movement_component, GameObject parent)
+        {
+            _positionComponent = object_position_component;
+            _movementComponent = object_movement_component;
             _parentGameObject = parent;
         }
 

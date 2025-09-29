8214e2518e3ac95e8328df4fbfcd4fd21bc35aba
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-29 09:55:04 +0200

    GameState: Création GameState de base

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/States/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3._1_RPG_game.States
+{
+    public abstract class GameState
+    {
+        //Nécessaire?
+
+        // public abstract void Enter();
+
+        // public abstract void Exit();
+    }
+}

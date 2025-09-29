6b6a0b90e395b9d51eb0dd0bc7abd492b83e7ddf
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-29 12:11:12 +0200

    StateMachine: Création StateMachine

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/StateMachine.cs
@@ -0,0 +1,31 @@
+﻿using Prog_3._1_RPG_game.States;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3._1_RPG_game
+{
+    internal class StateMachine
+    {
+        private IState _currentState;
+
+        public StateMachine(IState initial_state)
+        {
+            _currentState = initial_state;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void ChangeState(IState new_state)
+        {
+            _currentState.Exit();
+            _currentState = new_state;
+            _currentState.Enter();
+        }
+    }
+}

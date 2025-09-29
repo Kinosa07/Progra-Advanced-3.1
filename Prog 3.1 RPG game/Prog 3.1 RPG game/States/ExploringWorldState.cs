11bd1fdb9e384c9ce315d6006df1efe93dadf2d3
Kinosa07 <balleux.ethan@gmaiL.com>
2025-09-29 12:12:44 +0200

    ExploringWorldState: Ajout fonctions bases
    Ajout des fonctions necessaires à un IState et ajout d'un RenderManager et World Map en dépéndances

--- Prog 3.1 RPG game/Prog 3.1 RPG game/States/ExploringWorldState.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/States/ExploringWorldState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prog_3._1_RPG_game.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,38 @@ namespace Prog_3._1_RPG_game.States
 {
     internal class ExploringWorldState : IState
     {
+        private MapComponent _worldMap;
+        private RenderManager _renderManager;
 
+        public ExploringWorldState(MapComponent world_map, RenderManager render_manager)
+        {
+            _worldMap = world_map;
+            _renderManager = render_manager;
+        }
+
+        public void Enter()
+        {
+
+        }
+
+        public void Exit()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void ProcessInput()
+        {
+
+        }
+
+        public void Render()
+        {
+            _renderManager.Render(_worldMap);
+        }
     }
 }

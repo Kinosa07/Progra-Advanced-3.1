773f069ee3729894a05ffb26267ec8d9bc08d120
Kinosa07 <balleux.ethan@gmaiL.com>
2025-10-03 17:06:30 +0200

    Création InputComponent
    Créatio du Component pour gérer les Inputs du joueur.
    
    A chaque Update, le Component lis l'input joueur
    A chaque FixedUpdate, le Component assigne la dernière touche pressée à l'Event KeyPressed

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Components/InputComponent.cs
@@ -0,0 +1,60 @@
+﻿using Prog_3._1_RPG_game.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3._1_RPG_game.Components
+{
+    internal class InputComponent: Component
+    {
+        private GameObject _parent;
+        private EventManager _eventManager;
+        private ConsoleKey _lastKeyPressed;
+
+        public InputComponent(GameObject parent_object, EventManager event_manager)
+        {
+            _parent = parent_object;
+            _eventManager = event_manager;
+            _lastKeyPressed = ConsoleKey.RightWindows;
+            //Utilisation de RightWindows comme valeur "nulle". Dû à la fréquence d'uilisation de la touche dans un environnement de jeu
+        }
+
+        public override void Update (float time_since_last_update)
+        {
+            ReadInput();
+        }
+
+        public override void FixedUpdate(float fixed_time_for_new_update)
+        {
+            ProcessInput();
+        }
+
+        public override GameObject GetParentGameObject()
+        {
+            GameObject copy_of_parent = _parent;
+            return copy_of_parent;
+        }
+
+        public void ReadInput()
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                
+                _lastKeyPressed = key.Key;
+            }
+        }
+
+        public void ProcessInput()
+        {
+            if (_lastKeyPressed != ConsoleKey.RightWindows)
+            {
+                _eventManager.TriggerEvent(new KeyPressedEvent(_lastKeyPressed));
+
+                _lastKeyPressed = ConsoleKey.RightWindows;
+            }
+        }
+    }
+}

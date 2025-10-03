9f52833b62325ebe93d87e3dd329dcf2eee6a2c9
Kinosa07 <balleux.ethan@gmaiL.com>
2025-10-03 16:51:27 +0200

    KeyPressed Event: Création Event
    Création d'un Event lorsqu'une touche est pressée pour contenir la touche pressée

---
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Events/KeyPressedEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_3._1_RPG_game.Events
+{
+    internal class KeyPressedEvent
+    {
+        private ConsoleKey _keyPressed;
+
+        public KeyPressedEvent(ConsoleKey key_pressed)
+        {
+            _keyPressed = key_pressed;
+        }
+    }
+}

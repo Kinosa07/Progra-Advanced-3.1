1ea1247078820916f804dc47b48a4a8cbfc7b661
Kinosa07 <balleux.ethan@gmaiL.com>
2025-10-03 16:49:37 +0200

    EventManager: Ajout Param au Dictionnaire
    Ajout d'un paramètre GameEvent necessaire aux actions ajoutées dans le Dictionnaire (afin d'être utilisé dans les fonctions qui en découlent)
    + note personnelle

--- Prog 3.1 RPG game/Prog 3.1 RPG game/EventManager.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/EventManager.cs
@@ -9,20 +9,23 @@ namespace Prog_3._1_RPG_game
 {
     internal class EventManager
     {
-        private Dictionary<Type, List<Action>> _eventTable;
+        //Type comme Key pour permettre une simplicité de traversée grâce a des composants généric  
+        //List comme Value pour permettre à un event d'activer plusieurs actions (Ex: Tirer envoie un balle & réduit le nombre de balles dans un chargeur
+        //Action<GameEvent> pour s'assurer de pouvoir récupérer des infos transmises pas l'Event si nécessaire
+        private Dictionary<Type, List<Action<GameEvent>>> _eventTable;
 
         public EventManager()
         {
 
         }
 
-        public void RegisterToEvent<EVENT_TYPE>(Action action_linked_to_event) where EVENT_TYPE : GameEvent
+        public void RegisterToEvent<EVENT_TYPE>(Action<GameEvent> action_linked_to_event) where EVENT_TYPE : GameEvent
         {
             Type type_of_event = typeof(EVENT_TYPE);
 
             if (!(_eventTable.ContainsKey(type_of_event)))
             {
-                _eventTable.Add(type_of_event, new List<Action>());
+                _eventTable.Add(type_of_event, new List<Action<GameEvent>>());
             }
 
             _eventTable[type_of_event].Add(action_linked_to_event);
@@ -34,8 +37,8 @@ namespace Prog_3._1_RPG_game
 
             if(_eventTable.ContainsKey(type_of_event))
             {
-                foreach (Action action in _eventTable[type_of_event])
-                action();
+                foreach (Action<GameEvent> action in _eventTable[type_of_event])
+                action(game_event);
             }
         }
     }

a2725e5387607b94addf3fd1924cad563f3cb951
Kinosa07 <balleux.ethan@gmaiL.com>
2025-10-03 16:58:48 +0200

    KeyPressedEvent: Lien parent-enfant
    Mise a jout de l'état de parenté de KeyPressedEvent pour qu'il soit considéré comme un GameEvent

--- Prog 3.1 RPG game/Prog 3.1 RPG game/Events/KeyPressedEvent.cs
+++ Prog 3.1 RPG game/Prog 3.1 RPG game/Events/KeyPressedEvent.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Prog_3._1_RPG_game.Events
 {
-    internal class KeyPressedEvent
+    internal class KeyPressedEvent : GameEvent
     {
         private ConsoleKey _keyPressed;
 
